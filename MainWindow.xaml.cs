c8b09a99c0413d63be31e1f1717d1240d42d90e0
Taka005 <contact@takadev.jp>
2025-04-15 21:05:57 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhysicsEngineGUI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void NewFile_Click(object sender, RoutedEventArgs e){
+        MessageBox.Show("新規ファイルを作成します");
+    }
+
+    private void SaveFile_Click(object sender, RoutedEventArgs e){
+        MessageBox.Show("ファイルを保存します");
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e){
+        Application.Current.Shutdown();
+    }
+}
