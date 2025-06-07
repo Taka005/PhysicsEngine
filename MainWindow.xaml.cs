9b261a77fb136ebbb28912582d27beebfdc81eff
Taka005 <contact@takadev.jp>
2025-06-07 13:30:08 +0900

    MainWindowの描画設定とオプションを追加
    MainWindow.xamlでCanvasにClipToBounds属性を追加し、描画領域を制限しました。
    MainWindow.xaml.csではEngineOptionにfrictionとppsを追加し、isDebugModeをコメントアウトしました。
    新たにTriangleOption、LineOption、CurveOptionを追加し、エンジンにスポーンする処理を実装しました。
    NewFile_Clickメソッドで複数のCircleOptionを生成する処理を追加しましたが、一部はコメントアウトされています。
    全体的にコードの可読性と機能が向上しました。

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,12 +22,14 @@ public partial class MainWindow : Window{
         InitializeComponent();
 
         EngineOption engineOption = new EngineOption {
-            gravity = 100
+            gravity = 100,
+            friction = 0.01,
+            pps = 180,
         };
 
         this.engine = new Engine(engineOption);
 
-        this.engine.render.isDebugMode = true;
+        //this.engine.render.isDebugMode = true;
 
         this.engine.render.Width = MyCanvas.ActualWidth;
         this.engine.render.Height = MyCanvas.ActualHeight;
@@ -54,6 +56,15 @@ public partial class MainWindow : Window{
             stiffness = 0.8
         };
 
+        TriangleOption circleOption8 = new TriangleOption {
+            posX = 300,
+            posY = 100,
+            mass = 1,
+            size = 10,
+            stiffness = 0.8
+        };
+
+
         RopeOption circleOption3 = new RopeOption {
             startX = 100,
             startY = 150,
@@ -72,14 +83,85 @@ public partial class MainWindow : Window{
             width = 10
         };
 
+        LineOption circleOption6 = new LineOption {
+            startX = 10,
+            startY = 240,
+            endX = 10,
+            endY = 0,
+            width = 10
+        };
+
+
+        LineOption circleOption7 = new LineOption {
+            startX = 500,
+            startY = 240,
+            endX = 500,
+            endY = 0,
+            width = 10
+        };
+
+        CurveOption circleOption5 = new CurveOption {
+            startX = 500,
+            startY = 250,
+            middleX = 600,
+            middleY = 300,
+            endX = 500,
+            endY = 500,
+            width = 10
+        };
+
         engine.SpawnObject(circleOption1);
         engine.SpawnObject(circleOption4);
+        engine.SpawnObject(circleOption8);
         engine.SpawnGround(circleOption2);
-        engine.SpawnObject(circleOption3);
+        engine.SpawnGround(circleOption6);
+        engine.SpawnGround(circleOption7);
+        engine.SpawnGround(circleOption5);
+        //engine.SpawnObject(circleOption3);
     }
 
     private void NewFile_Click(object sender, RoutedEventArgs e){
-        MessageBox.Show("新規ファイルを作成します");
+        //MessageBox.Show("新規ファイルを作成します");
+
+        for(int i = 0;i < 50;i++) {
+            CircleOption circleOption = new CircleOption {
+                posX = 90 + 10*i,
+                posY = 50,
+                mass = 10,
+                diameter = 10,
+                stiffness = 1
+            };
+
+            engine.SpawnObject(circleOption);
+
+            CircleOption circleOption2 = new CircleOption {
+                posX = 90 + 5 * i,
+                posY = 60,
+                mass = 10,
+                diameter = 5,
+                stiffness = 0.8
+            };
+
+            CircleOption circleOption3 = new CircleOption {
+                posX = 90 + 5 * i,
+                posY = 70,
+                mass = 1,
+                diameter = 5,
+                stiffness = 0.8
+            };
+
+            CircleOption circleOption4 = new CircleOption {
+                posX = 90 + 5 * i,
+                posY = 80,
+                mass = 1,
+                diameter = 5,
+                stiffness = 0.8
+            };
+
+            //engine.SpawnObject(circleOption2);
+            //engine.SpawnObject(circleOption3);
+            //engine.SpawnObject(circleOption4);
+        }
     }
 
     private void SaveFile_Click(object sender, RoutedEventArgs e){
